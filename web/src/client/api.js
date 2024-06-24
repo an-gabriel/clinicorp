5253a061a78f7e8af30891fb3b55917e357a1a83
Gabriel Aquino <gabriel.aquino@contraclabs.tech>
2024-06-24 10:48:13 -0300

    chore: add interceptors axios

--- web/src/client/api.js
+++ web/src/client/api.js
@@ -7,6 +7,14 @@ const api = axios.create({
   },
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export const registerUser = async (userData) => {
   try {
     const response = await api.post("/auth/register", userData);
@@ -24,3 +32,40 @@ export const loginUser = async (userData) => {
     throw error;
   }
 };
+
+export const listProjects = async () => {
+  try {
+    const response = await api.get("/project");
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const sendProjectData = async (project, currentProject) => {
+  if (currentProject) {
+    return await api.put(`/project/${currentProject.id}`, project);
+  } else {
+    return await api.post("/project", project);
+  }
+};
+
+export const updateProject = async (projectId, projectData) => {
+  try {
+    const response = await api.put(`/project/${projectId}`, projectData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteProject = async (projectId) => {
+  try {
+    const response = await api.delete(`/project/${projectId}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default api;
